c2beee7ee1cf57d8098087940a9b69d6181c2483
Usama1238 <68499486+Usama1238@users.noreply.github.com>
2023-02-06 15:19:43 +0500

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -29,7 +29,7 @@ def init_net1(net1, init_type='normal', init_gain=0.02, gpu_ids=[], debug=False,
         # if not amp:
         # net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs for non-AMP training
     if initialize_weights:
-        init_weights(net1, init_type, init_gain=init_gain, debug=debug)
+        init_weights(net1, init_type, init_gain=init_gain)
     return net1    
 
 
