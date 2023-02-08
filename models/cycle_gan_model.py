ff1cacd7b22b23499ea45ed63e23cb9cdee0bdc6
Usama1238 <68499486+Usama1238@users.noreply.github.com>
2023-02-08 11:37:53 +0500

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -224,7 +224,7 @@ class CycleGANModel(BaseModel):
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         #summary(self.netG_B)
         self.netF = define_F(opt.input_nc, opt.netF, opt.norm,not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,opt)
-        #summary(self.netF)
+        summary(self.netF)
 
         if self.isTrain:  # define discriminators
             self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
