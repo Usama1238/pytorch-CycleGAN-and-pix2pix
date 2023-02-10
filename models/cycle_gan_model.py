538116d0857949d5cea395a6498be7e094bf86f9
Usama1238 <68499486+Usama1238@users.noreply.github.com>
2023-02-10 12:11:37 +0500

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -81,9 +81,9 @@ class CycleGANModel(BaseModel):
         # The naming is different from those used in the paper.
         # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
         self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,opt)
         self.netG_B = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,opt)
         #summary(self.netG_B)
         self.netF =  networks.define_F(opt.input_nc,'mlp_sample', opt.norm,not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,opt)
         #summary(self.netF)
